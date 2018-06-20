15cbd6a1aa36987b0200e867d6bfb190c9a05909
akshaypalshikar <aksmakesit@gmail.com>
2018-06-20 01:42:32 -0700

    Person module added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     
         return (
       <div className="App">
-       <h1>Hi! This is a react App.</h1>
+       <h1>Hi {Person.name}! This is a react App.</h1>
        <p>This is really working!</p>
+       <Person />
       </div>
     );
   
