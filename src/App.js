e5551ac1b898a08387bf153aabfdd4ec9f0e6928
akshaypalshikar <aksmakesit@gmail.com>
2018-07-03 18:57:48 -0700

    inline and .css types styling

--- src/App.js
+++ src/App.js
@@ -33,12 +33,19 @@ class App extends Component {
     })
   }
   render() {
+    const style = {
+      backgroundColor : 'white',
+      font : 'inherit',
+      border : '1px solid blue',
+      padding : '8px',
+      cursor : 'pointer'
+    };
         return (
       <div className="App">
        <h1>Hi! This is a react App.</h1>
        <p>This is really working!</p>
-       <button onClick={this.switchNameHandler.bind(this,"Akshay Palshikar")}>Switch Name Method1</button>
-       <button onClick={()=>this.switchNameHandler("Poorva!")}>Switch Name Method2 - ArrowF</button>
+       <button style={style} onClick={this.switchNameHandler.bind(this,"Akshay Palshikar")}>Switch Name Method1</button>
+       <button style={style} onClick={()=>this.switchNameHandler("Poorva!")}>Switch Name Method2 - ArrowF</button>
        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this,"Akshay!")}>My hobbies : Coding</Person>
        <Person  name={this.state.persons[1].name} 
                 age={this.state.persons[1].age} 
