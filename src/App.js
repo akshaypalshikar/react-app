79f0dbcdd43189580fb8485a77e558573890c6a4
akshaypalshikar <aksmakesit@gmail.com>
2018-07-13 23:33:51 -0700

    added radium and pseudo selector

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -45,7 +46,11 @@ class App extends Component {
       font : 'inherit',
       border : '1px solid blue',
       padding : '8px',
-      cursor : 'pointer'
+      cursor : 'pointer',
+      ':hover':{
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
     };
 
 
@@ -62,6 +67,10 @@ class App extends Component {
                     </div>
                   );
                   style.backgroundColor='red';
+                  style[':hover']={
+                    backgroundColor:'salmon',
+                    color:'black'
+                  }
     }
 
     const cssClasses = [];
@@ -87,4 +96,4 @@ class App extends Component {
   
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Person from './Person.css';
+import Radium from 'radium';
 
 const person = (props) => {
 console.log(props);
@@ -12,4 +13,4 @@ return (
 );
 }
 
-export default person;
+export default Radium(person);
