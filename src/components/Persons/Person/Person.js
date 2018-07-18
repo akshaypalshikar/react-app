b1b09b3dd0f261e2e761e46df27237991265663e
akshaypalshikar <aksmakesit@gmail.com>
2018-07-18 00:20:07 -0700

    created better structure

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,11 +3,6 @@ import cssClasses from './Person.css';
 
 
 const person = (props) => {
-    const rnd = Math.random();
-    if( rnd > 0.7 ){
-        throw new Error("Something went wrong!");
-    }
-
     return (
         <div className={cssClasses.Person}>
             <p onClick={props.clicked}> I'm a {props.name} and I am {props.age} years old!</p>
