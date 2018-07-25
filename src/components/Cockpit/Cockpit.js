cae8409ba512fde702b30ad3050342085b10d5b7
akshaypalshikar <aksmakesit@gmail.com>
2018-07-24 20:59:30 -0700

    added higher order component as a wrapper

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Persons from './../Persons/Persons'
 import cockpitCssClasses from './Cockpit.css'
-
+import Aux from '../../hoc/Aux';
 
 const cockpit = (props) => {
-    let btnClass = '';
+    let btnClass = cockpitCssClasses.Button;
     let persons = [];
     if(props.showPersons){
         persons = (<Persons persons={props.persons}      
           deletePersonHandler = {props.deletePersonHandler}
           nameChangedHandler = {props.nameChangedHandler}
         />);
-        btnClass = cockpitCssClasses.Red;
+        btnClass = [cockpitCssClasses.Button,cockpitCssClasses.Red].join(' ');
     }
 
     const assignedCssClasses = [];
@@ -23,12 +23,12 @@ const cockpit = (props) => {
     }
 
     return (
-        <div className={cockpitCssClasses.Cockpit}>
+        <Aux>
             <h1>{props.title}</h1>
             <p className={assignedCssClasses.join(' ')}>This is really working!</p>
             <button className={btnClass} onClick={props.togglePersonsHandler.bind(this)}>Toggle Persons</button>
             {persons}
-        </div>
+        </Aux>
     )};
 
 export default cockpit;
